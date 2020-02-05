0f1ab30b52f24aafa6f8393102923749000bc570
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-05 20:48:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,16 @@
 class CashRegister
 
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :transactions
 
   def initialize(discount = 0)
     @total, @discount, @items = 0, discount, []
+    @transactions = {}
   end
 
   def add_item(title, price, quantity = 1)
     @total += (price * quantity)
     quantity.times{ @items.push(title)}
+    @transactions[title] ||= { "quantity" = quantity, price = price}
   end
 
   def apply_discount
@@ -21,6 +23,6 @@ class CashRegister
   end
 
   def void_last_transaction
-    @
+    
   end
 end
