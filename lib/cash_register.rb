deac9c61feb8f414f80250c4967f53bb72dc526a
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-05 20:08:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += (price * quantity)
-    do quantity.times 
+    do quantity.times
       @items.push(title)
     end
   end
