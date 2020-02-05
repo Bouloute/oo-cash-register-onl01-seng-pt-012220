a4ddda4231577c486204fba89da5dd9206483a32
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-05 19:50:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount = 0 
+    if @discount = 0
       return "There is no discount to apply."
     end
     @total = @total - @total * (discount/100.to_f)
